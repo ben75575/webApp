c07271fb5daa9d2399047112cda82dba861839ca
ben75575 <benjamin.macholz@stud.fra-uas.de>
2024-11-17 21:34:31 +0100

    Added first Spring Boot Project

---
+++ Projekt1/src/main/java/edu/fra/uas/Projekt1/Projekt1Application.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.Projekt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt1Application.class, args);
+	}
+
+}
---
+++ Projekt1/src/test/java/edu/fra/uas/Projekt1/Projekt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.Projekt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
